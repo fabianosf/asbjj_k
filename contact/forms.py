6141be6f65610d80bc0f87882f93486a58238c53
Fabiano Freitas <fabiano.freitas@gmail.com>
2024-08-26 00:52:40 -0300

    Resolve conflitos de merge em contact/forms.py

--- contact/forms.py
+++ contact/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class ContactForm(forms.Form):
-<<<<<<< HEAD
+
     nome = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Seu Nome'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Seu Email'}))
     mensagem = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Sua Mensagem'}))
-=======
+
     name = forms.CharField(
         max_length=100,
         widget=forms.TextInput(attrs={'class': 'form-control custom-input', 'placeholder': 'Nome'}),
@@ -40,4 +40,4 @@ class ContactForm(forms.Form):
             raise forms.ValidationError('Todos os campos devem ser preenchidos.')
 
         return cleaned_data
->>>>>>> refs/remotes/origin/master
+
