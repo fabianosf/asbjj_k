5aea13578cd7a94e6c6c640a65474c363e3b6c8c
Fabiano Freitas <fabiano.freitas@gmail.com>
2024-08-26 01:13:56 -0300

    resolvendo conflitos

--- asbjj/settings.py
+++ asbjj/settings.py
@@ -129,18 +129,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'static'),
-#    os.path.join(BASE_DIR, 'contact/static'),
-    
 #]
+
+
+
+'''
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'contact/static'),
+    os.path.join(BASE_DIR, 'accounts/static'),
+    
+]
+'''
 
 
 
